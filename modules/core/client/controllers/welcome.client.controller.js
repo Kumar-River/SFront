04364519b194255b1ccf98e9d1a2985e3618bb2b
kumar <kumar@riverstonetech.com>
2018-06-13 10:05:00 +0530

    first commit

---
+++ modules/core/client/controllers/welcome.client.controller.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('core')
+    .controller('WelcomeController', WelcomeController);
+
+  function WelcomeController() {
+    var vm = this;
+  }
+}());
