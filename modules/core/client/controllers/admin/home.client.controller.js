fdb38c0a47ae180a8500e08534076fd11e41d330
uazeem <ubaidulla@riverstonetech.com>
2018-06-20 13:03:27 +0530

    Implemented signin feature

--- modules/core/client/controllers/admin/home.client.controller.js
+++ modules/core/client/controllers/admin/home.client.controller.js
@@ -1,11 +1,39 @@
-(function () {
+(function() {
   'use strict';
 
   angular
     .module('core')
     .controller('AdminLoginController', AdminLoginController);
 
-  function AdminLoginController() {
+  AdminLoginController.$inject = ['Authentication', '$scope', '$state', 'Notification', 'UsersService'];
+
+  function AdminLoginController(Authentication, $scope, $state, Notification, UsersService) {
     var vm = this;
+
+    vm.signin = signin;
+    vm.authentication = Authentication;
+
+    function signin(isValid) {
+      if (!isValid) {
+        $scope.$broadcast('show-errors-check-validity', 'vm.userForm');
+
+        return false;
+      }
+
+      UsersService.userSignin(vm.credentials)
+        .then(onUserSigninSuccess)
+        .catch(onUserSigninError);
+    }
+
+    function onUserSigninSuccess(response) {
+      // If successful we assign the response to the global user model
+      vm.authentication.user = response;
+      Notification.info({ message: 'Welcome ' + response.firstName });
+      localStorage.setItem('log_in_time', new Date().getTime());
+    }
+
+    function onUserSigninError(response) {
+      Notification.error({ message: response.data.message, title: '<i class="glyphicon glyphicon-remove"></i> Signin Error!', delay: 6000 });
+    }
   }
 }());
